bf4aa2bcf52377327c3e967f64c3bca5c86c1992
robert.kulicki@softwarehut.com <robert.kulicki@softwarehut.com>
2020-02-11 11:51:16 +0100

    Splash screen configuration on Android platform - part 2

--- App.js
+++ App.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component {
   componentDidMount() {
     SplashScreen.hide();
@@ -17,7 +12,6 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
@@ -28,17 +22,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#3C69FF',
+    backgroundColor: '#fff',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    color: '#fff',
+    color: '#3C69FF',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#fff',
+    color: '#3C69FF',
     marginBottom: 5,
   },
 });
