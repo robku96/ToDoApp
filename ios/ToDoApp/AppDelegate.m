c184493d26559bc0aab96f5a57954cc665d26f66
Robert Kulicki <robert.kulicki@MKs-Mac-mini-2019.local>
2020-02-11 13:15:35 +0100

    Splash screen configuration on ios platform

--- ios/ToDoApp/AppDelegate.m
+++ ios/ToDoApp/AppDelegate.m
@@ -14,6 +14,8 @@
 #import <UMReactNativeAdapter/UMNativeModulesProxy.h>
 #import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 
+#import "RNSplashScreen.h"
+
 @implementation AppDelegate
 
 @synthesize window = _window;
@@ -32,7 +34,9 @@
   [self.window makeKeyAndVisible];
 
   [super application:application didFinishLaunchingWithOptions:launchOptions];
-
+  
+  
+  [RNSplashScreen show];
   return YES;
 }
 
